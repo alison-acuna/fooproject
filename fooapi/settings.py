7dd479e218b073b386bc789cac2bf191f71f7e90
Alison <aacuna136@gmail.com>
2017-10-05 19:08:00 -0700

    Assignment 4 Complete

--- fooapi/settings.py
+++ fooapi/settings.py
@@ -74,15 +74,15 @@ WSGI_APPLICATION = 'fooapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgresql,
-        'USER': 'postgresql,
-        'HOST': 'db',
-        'PORT': 5432,
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgresql,
+#         'USER': 'postgresql,
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
 
 
 # Password validation
@@ -122,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+from fooapi.foo_api_settings import *
